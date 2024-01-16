4f1db2117f9fb68f424a0547255a7b4576bbdcb6
adriancr1 <adrian.casal.rodriguez@gmail.com>
2024-01-16 20:15:59 +0100

    scripts updated

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -32,7 +32,7 @@ export class Server {
         })
         
         this.app.listen(this.port, () => {
-            console.log(`Server running on ${ this.port }`);
+            console.log(`Server running on port ${ this.port }`);
         });
     }
 }
